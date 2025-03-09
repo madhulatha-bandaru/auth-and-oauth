be1baa8264c1ed2888fca2e9d2230f791a577665
Madhu Latha Bandaru <apple@Apples-MacBook-Air.local>
2025-03-09 18:33:24 +0530

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import About from './pages/About'
+import Profile from './pages/Profile';
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/sign-in" element={<Signin/>}/>
+        <Route path="/sign-up" element={<Signup/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
