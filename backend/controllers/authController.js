4b7c175bf04a98bbe658e442750bf3940b66f437
madhulatha-bandaru <madhulatha14bandaru@gmail.com>
2025-03-09 22:51:47 +0530

    updated signup page

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,15 +1,15 @@
 import User from "../models/userSchema.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req,res,next) => {
-  const {username, email, password} = req.body;
-  const hashedPassword = bcryptjs.hashSync(password, process.env.KEY);
-  const newUser = new User({username,email,password: hashedPassword });
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json({message : "Account created successfully!"})
-  } catch(error) {
+    res.status(201).json({ message: "Account created successfully!" });
+  } catch (error) {
     next(error);
   }
-}
+};
