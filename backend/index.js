a8872e523cab6e66e1751798ba6c0d606b21b670
madhulatha-bandaru <madhulatha14bandaru@gmail.com>
2025-03-09 19:22:46 +0530

    connected to db

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Successfully connected to the database!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
   console.log("Server listening on port 3000!");
-})
+});
