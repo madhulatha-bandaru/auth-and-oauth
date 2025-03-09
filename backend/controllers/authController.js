8661b7835dc20d67c728e1e7f714923ae0b9b445
madhulatha-bandaru <madhulatha14bandaru@gmail.com>
2025-03-09 20:34:05 +0530

    added middleware

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,14 +1,15 @@
 import User from "../models/userSchema.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req,res) => {
+export const signup = async (req,res,next) => {
   const {username, email, password} = req.body;
-  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, process.env.KEY);
   const newUser = new User({username,email,password: hashedPassword });
   try {
     await newUser.save();
     res.status(201).json({message : "Account created successfully!"})
   } catch(error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 }
